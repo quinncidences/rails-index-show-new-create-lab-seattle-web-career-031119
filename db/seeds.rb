2d86c0e9de1ec3eeba967b13924eef2ee29fe812
quinncidences <qrc.inpa@gmail.com>
2019-04-08 12:28:09 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "329498", store: "Mels")
+Coupon.create(coupon_code: "112412", store: "Pats")
+Coupon.create(coupon_code: "34411", store: "Olgas")
